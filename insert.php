4e8b1c7257d5546c60716054b93a1969c0524bf3
OrangeProject <nakayamahideki@chuuzan-no-MacBook-Air.local>
2017-09-28 23:21:44 +0900

    no message

--- insert.php
+++ insert.php
@@ -1,21 +1,24 @@
+
 <?php
+
+
 //1. POST受信
-$name = $_POST["name"];
-$email = $_POST["email"];
-$naiyou = $_POST["naiyou"];
+$title = $_POST["title"];
+$url = $_POST["url"];
+$comment = $_POST["comment"];
 
 //2. DB接続
 try {
-  $pdo = new PDO('mysql:dbname=gs_db36;charset=utf8;host=localhost','root', '');
+  $pdo = new PDO('mysql:dbname=gs_db;charset=utf8;host=localhost','root', '');
 } catch (PDOException $e) {
   exit('DbConnectError:'.$e->getMessage());
 }
 
 //３．SQL作成実行
-$stmt = $pdo->prepare("INSERT INTO gs_an_table(id, name, email, naiyou, indate )VALUES(NULL, :name, :email, :naiyou, sysdate())");
-$stmt->bindValue(':name', $name, PDO::PARAM_STR);
-$stmt->bindValue(':email', $email, PDO::PARAM_STR);
-$stmt->bindValue(':naiyou', $naiyou, PDO::PARAM_STR);
+$stmt = $pdo->prepare("INSERT INTO gs_bm_table(id, title, url, comment, regist_date)VALUES(NULL, :title, :url, :comment, sysdate())");
+$stmt->bindValue(':title', $title, PDO::PARAM_STR);
+$stmt->bindValue(':url', $url, PDO::PARAM_STR);
+$stmt->bindValue(':comment', $comment, PDO::PARAM_STR);
 $status = $stmt->execute();
 
 //４．エラー処理
@@ -28,4 +31,21 @@ if($status==false){
   exit;
 
 }
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>課題7</title>
+    
+    <link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+
+</body>
+</html>
